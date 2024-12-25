2a74f723bbaff1581f29baa426967167f1436b9b
Quentin Adam <quentinadam@gmail.com>
2024-12-25 23:08:36 +0100

    Minor documentation changes. Bump version to v0.1.2.

--- base58.ts
+++ base58.ts
@@ -36,18 +36,30 @@ function convert({ inputBase, outputBase, input }: {
 }
 
 function getAlphabet(alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz') {
-  assert(alphabet.length === 58, `Invalid alphabet ${alphabet}`);
+  assert(alphabet.length === 58, 'Alphabet must be 58 characters long');
+  assert(new Set(alphabet).size === 58, 'Alphabet must not contain duplicate characters');
   return alphabet;
+}
+
+/** Options for {@linkcode encode} and {@linkcode decode}. */
+export interface Options {
+  /**
+   * The alphabet to use.
+   * The alphabet must be a string of 58 unique characters.
+   *
+   * @default {'123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'}
+   */
+  alphabet?: string;
 }
 
 /**
  * Encodes a Uint8Array buffer into a base58 string.
  *
  * @param buffer The buffer to encode.
- * @param options Optionally specify the alphabet to use. The alphabet must be a 58-character string.
+ * @param options The options to use for encoding.
  * @returns The base58 encoded string.
  */
-export function encode(buffer: Uint8Array, options?: { alphabet?: string }): string {
+export function encode(buffer: Uint8Array, options?: Options): string {
   const alphabet = getAlphabet(options?.alphabet);
   const output = convert({ input: buffer, inputBase: 256, outputBase: 58 });
   return output.map((digit) => require(alphabet[digit])).join('');
@@ -57,10 +69,10 @@ export function encode(buffer: Uint8Array, options?: { alphabet?: string }): str
  * Decodes a base58 encoded string into a Uint8Array buffer.
  *
  * @param string The base58 encoded string.
- * @param options Optionally specify the alphabet to use. The alphabet must be a 58-character string.
+ * @param options The options to use for decoding.
  * @returns The decoded buffer.
  */
-export function decode(string: string, options?: { alphabet?: string }): Uint8Array {
+export function decode(string: string, options?: Options): Uint8Array {
   const alphabet = getAlphabet(options?.alphabet);
   const map = new Map(Array.from(alphabet).map((character, index) => [character, index]));
   const input = Array.from(string).map((character) => require(map.get(character), `Invalid character ${character}`));
